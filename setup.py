993442c4564d6f9f60eef3ff6b621b3784cef437
travis <travis@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-05-22 15:43:42 +0000

    set version to proper number and use versions in 'install_requires' list

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'enthought.debug',
-    version = '1.1.0',
+    version = '2.0',
     description  = 'Frame Based Inspector - Traits based debugging tool',
     author       = 'Enthought, Inc',
     author_email = 'info@enthought.com',
@@ -12,8 +12,8 @@ setup(
     packages = find_packages(),
     include_package_data = True,
     install_requires = [
-        "enthought.traits", 
-        "enthought.pyface",
+        "enthought.traits==2.0", 
+        "enthought.pyface==2.0",
     ],
     namespace_packages = [
         "enthought",

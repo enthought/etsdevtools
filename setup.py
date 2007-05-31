1f25d5264e26c030dee23b1e3e4d7bf20c533532
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-05-31 21:57:49 +0000

    * Declared these packages to be beta releases with versions like 2.0b1 -- they aren't complete, bugfree, and stable quite yet. * Updated dependencies to work with version between 2.0b1 and anything less than 3.0  (should this be between 2.0b1 and <2.1?)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'enthought.debug',
-    version = '2.0',
+    version = '2.0b1',
     description  = 'Frame Based Inspector - Traits based debugging tool',
     author       = 'Enthought, Inc',
     author_email = 'info@enthought.com',
@@ -12,8 +12,8 @@ setup(
     packages = find_packages(),
     include_package_data = True,
     install_requires = [
-        "enthought.traits==2.0", 
-        "enthought.pyface==2.0",
+        "enthought.traits >=2.0b1, <3",
+        "enthought.pyface >=2.0b1, <3",
     ],
     namespace_packages = [
         "enthought",

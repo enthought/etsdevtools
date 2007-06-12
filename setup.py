3ddad94c606fe134c566795f1e2790cf63cc509d
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-06-12 12:35:20 +0000

    * Refactored setup.py's to enable release and dev builds to work with the same set of declared packages and version numbers. * Renamed ets as version 2.5 to ensure its higher than the 2.4 version already available from the egg repo.

--- setup.py
+++ setup.py
@@ -1,5 +1,21 @@
 from setuptools import setup, find_packages
 
+
+# Function to convert simple ETS component names and versions to a requirements
+# spec that works for both development builds and stable builds.
+def gendeps(list):
+    return ['%s >=%s.dev, <=%s.dev' % (p,min,max) for p,min,max in list]
+
+# Declare our installation requirements.
+install_requires = gendeps([
+    ("enthought.envisage", "2.0b1", "3"),
+    ("enthought.logger", "2.0b1", "3"),
+    ("enthought.pyface", "2.0b1", "3"),
+    ("enthought.traits", "2.0b1", "3"),
+    ])
+print 'install_requires:\n\t%s' % '\n\t'.join(install_requires)
+
+
 setup(
     name = 'enthought.debug',
     version = '2.0b2',
@@ -11,12 +27,13 @@ setup(
     zip_safe     = False,
     packages = find_packages(),
     include_package_data = True,
-    install_requires = [
-        "enthought.envisage >=2.0b1, <3",
-        "enthought.logger >=2.0b1, <3",
-        "enthought.pyface >=2.0b1, <3",
-        "enthought.traits >=2.0b1, <3",
-    ],
+    install_requires = install_requires,
+    extras_require = {
+        # All non-ets dependencies should be in this extra to ensure users can
+        # decide whether to require them or not.
+        'nonets': [
+        ],
+    },
     namespace_packages = [
         "enthought",
     ],

72a6b1bcb8ed959cc702cf1417fba64d3928f089
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-06-21 07:34:16 +0000

    * Moved the debug plugin to an enthought.plugins.debug egg in order to remove the dependency on envisage.

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ def gendeps(list):
 
 # Declare our installation requirements.
 install_requires = gendeps([
-    ("enthought.envisage", "2.0b1", "3"),
     ("enthought.pyface", "2.0b1", "3"),
     ("enthought.traits", "2.0b1", "3"),
     ])

6726659c129721954bd257439fe730718b959399
swisher <swisher@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-04-21 22:41:38 +0000

    Replace --project/-j option with --use_setup/-t, which searches for subdirectories containing setup.py files.

--- enthought/endo/scripts/ets_endo.py
+++ enthought/endo/scripts/ets_endo.py
@@ -2,38 +2,18 @@
 Copyright 2008 Enthought, Inc.
 License: BSD Style
 
-This script figures out appropriate subdirectories of a specified or
-working directory to document, infers the structure of package names and 
+This script figures out appropriate subdirectories of specified directories or
+the working directory to document, infers the structure of package names and 
 subdirectories, and runs endo.py, passing all the package info that it found, 
 along with any endo arguments passed to it on the command line.
 
 By default, the script simply finds subdirectories of the specified
 directory that contain packages, as evidenced by an __init__.py file, and
-documents those. You can use the --project (or -j) option to specify a directory
-as containing a setuptools-based project; in that case, the script documents
-only those packages specified by the setup.py for that project. 
+documents those. Alternatively, you can use the --use-setup (or -t) option
+to specify that it searches for setup.py files, and uses only the packages
+specified by the setup.py files that it finds.
 
-If you use the --project/-j option, the name and version number of the
-specified project(s) are inserted into the header of the output, using
-endo's --header option. However, explicitly using the --header or
---header_filename option replaces any header created by this feature.
 
-The script assumes that the directories follow the naming and structure 
-conventions of the "enthought" repository, and therefore may not be appropriate
-for other repositories.
-
-Specifically, it assumes that the project directories are structured as follows::
-
-    Project_M.m/
-        setup.py
-        pkg/
-            __init__.py
-            subpkg/
-                __init__.py
-                
-There may be multiple *pkg* directories under a project directory (though in the
-Enthought repository, there is usually only one, named 'enthought'), and there
-may be multiple *subpkg* directories under a project directory.
 """
 import optparse
 import os
@@ -47,15 +27,19 @@ class ETSEndoException(Exception): pass
 
 ENDO = "endo.py"
 
-header_template = "--header=<p>API Documentation set for: %s</p>"
-
-def _get_package_dirs(apath):
+def _get_usable_dirs(apath, setup=False):
+    """ Returns a list of subdirectories of *apath* that contain either
+    ``__init__.py`` if *setup* is False, or ``setup.py`` if *setup* is True.
+    """
+    # Look for setup.py if setup is True, otherwise look for __init__.py
+    file_to_check = setup and 'setup.py' or '__init__.py'
+        
     result = [ ]
-    stack = [ apath ]
+    stack = [ os.path.abspath(apath) ]
     while len(stack) > 0:
         top = stack.pop()
-        if os.access(os.path.join(top, '__init__.py'), os.R_OK):
-            # __init__.py exists -- dir is a package
+        if os.access(os.path.join(top, file_to_check), os.R_OK):
+            # file exists -- dir is usable
             result.append(top)
 
         # check sub directories
@@ -65,6 +49,11 @@ def _get_package_dirs(apath):
     return result
 
 def _get_project_packages(apath):
+    """ Returns a list of package names referenced by ``setup.py`` in *apath*.
+    
+    The list includes the packages referenced by the 'packages' and
+    'namespace_packages' parameters of setup().
+    """
     # Based on enthought.svn.project_set.ProjectSet._get_project_info_from_svn()
     packages = []
     setup_file = os.path.join(apath, 'setup.py')
@@ -81,12 +70,16 @@ def _get_project_packages(apath):
             raise ETSEndoException('An error occurred while reading %s.\n The exception was: %s' % (setup_file, str(e)))
         
         try:
+            # Distutils expects setup.py to be run from where it lives
+            oldwd = os.getcwd()
+            os.chdir(apath)
             kwds = _safe_setup_execution(setup)
         except Exception, e:
             raise ETSEndoException('Unable to parse %s.\ The exception was: %s' % (setup_file, str(e)))
-        
-        if 'namespace_packages' in kwds:
-            packages.extend(kwds['namespace_packages'])
+        finally:
+            os.chdir(oldwd)
+        #if 'namespace_packages' in kwds:
+        #    packages.extend(kwds['namespace_packages'])
         if 'packages' in kwds:
             packages.extend(kwds['packages'])
             
@@ -129,11 +122,9 @@ def _safe_setup_execution(setup):
         try:
             scope = {'__file__':'setup.py', '__name__':'__main__'}
             exec setup in scope
-            #print "Fully executed setup.py"
         except:
             scope = {'__file__':'setup.py'}
             exec setup in scope
-            #print "Executed setup.py but not as __main__"
     finally:
         sys.stdout = sys.__stdout__
         sys.stderr = sys.__stderr__
@@ -154,48 +145,54 @@ def main():
     """ Entry point for the installed script.
     """
     ropts = []
-    header = ""
+
     # Parse the user's command line
     parser = optparse.OptionParser(
         version = '0.1',
         description = ('Runs endo on packages within a directory.'
             'If no path is explicitly specified, then the current directory '
             'is treated as the root of the tree to be documented.'),
-        usage = '%prog [endo_options] [path]'
+        usage = '%prog [endo_options] [path1 path2 ... pathN]'
         )
     add_endo_options(parser)
     group = optparse.OptionGroup(parser, "%prog-specific Options", "Not passed through to endo")
     group.add_option("-j", "--project", dest="project_list", action="append",
                      metavar="PATH", default=[], 
                      help="Root of Setuptools-based project located at PATH")
+    group.add_option("-t", "--use-setup", dest="use_setup", action="store_true",
+                     default=False, 
+                     help="Look for Setuptools-based projects")
     parser.add_option_group(group)
     options, args = parser.parse_args()
     if len(args) < 1:
-        runpath = os.getcwd()
+        pathlist = [ os.getcwd() ]
     else:
-        runpath = os.path.abspath(args[-1])
-        
+        pathlist = [ os.path.abspath(arg) for arg in args ]
+    
     if options.project_list != []:
         packages = []
         for proj in options.project_list:
-            if options.verbose:
-                sys.stdout.write("Looking for packages in project %s.\n" % proj)
-            if not os.path.isabs(proj):
-                proj = os.path.join(runpath, proj)
             try:
                 packages = _get_project_packages(proj)
             except Exception, e:
                 print str(e)
             for pkg in packages:
-                ropts.append('-r%s=%s' % (pkg, os.path.join(runpath, proj, pkg.replace('.', os.sep))))
-
-        if (not options.header) and (not options.header_filename):
-            header = header_template % ', '.join(proj.replace("_", " ") for proj in options.project_list)
+                ropts.append('-r%s=%s' % (pkg, os.path.join(proj, pkg.replace('.', os.sep))))
     else:
-        ropts.extend( ['-r%s' % pdir for pdir in _get_package_dirs(runpath) ] )
-        
-    #ropts.sort(reverse=True) # FIXME: Hack that puts the traits packages in the desired order
-    
+        for path in pathlist:
+            usable_dirs = _get_usable_dirs(path, options.use_setup)
+            if options.use_setup:
+                for adir in usable_dirs:
+                    packages = []
+                    try:
+                        packages.extend( _get_project_packages(adir) )
+                    except Exception, e:
+                        print str(e)
+                    for pkg in packages:
+                        ropts.append('-r%s=%s' % (pkg, os.path.join(adir, pkg.replace('.', os.sep))))
+            else:
+                ropts.extend( ['-r%s' % pdir for pdir in usable_dirs ] )
+                
     endo_args = []
     skip = False
     for arg in sys.argv[1:]: # Remove options endo doesn't understand
@@ -204,13 +201,17 @@ def main():
             continue
         elif arg.startswith('-j') or arg.startswith('--project'):
             continue
+        elif (arg == '-t') or (arg == '--use-setup'):
+            continue
+        elif arg in args:
+            continue
         if not skip:
             endo_args.append(arg)
         else:
             skip = False
 
     loc = os.path.dirname(os.path.abspath(__file__))
-    endo_cmd = [ sys.executable, os.path.join(loc, ENDO)] + endo_args + ropts + [header]
+    endo_cmd = [ sys.executable, os.path.join(loc, ENDO)] + endo_args + ropts
     if options.verbose:
         sys.stdout.write("Calling: %s\n" % endo_cmd)
     ret = subprocess.call(endo_cmd)

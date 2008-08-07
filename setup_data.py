260f4f16c75674e314342869a8767280d4f3ea1b
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-08-07 19:27:54 +0000

    * Updated to match version numbers for ETS 3.0.0b2 release.

--- setup_data.py
+++ setup_data.py
@@ -14,15 +14,15 @@ def etsdep(p, min, max=None, literal=False):
 
 
 # Declare our ETS project dependencies.
-APPTOOLS = etsdep('AppTools', '3.0.0b1')  # import of enthought.io in enthought.developer
-ENTHOUGHTBASE = etsdep('EnthoughtBase', '3.0.0b1')
-ENTHOUGHTBASE_UI = etsdep('EnthoughtBase[ui]', '3.0.0b1')  # e.util.wx.* imported in developer
-ENVISAGECORE = etsdep('EnvisageCore', '3.0.0b1')
-ENVISAGEPLUGINS = etsdep('EnvisagePlugins', '3.0.0b1')  # -- imported only in enthought.gotcha
-TRAITSBACKENDWX = etsdep('TraitsBackendWX', '3.0.0b1')  # -- imported mostly by enthought.developer, but one from enthought.gotcha
-TRAITSGUI = etsdep('TraitsGUI', '3.0.0b1')
-TRAITSGUI_DOCK = etsdep('TraitsGUI[dock]', '3.0.0b1')  # -- imported only by enthought.developer.
-TRAITS_UI = etsdep('Traits[ui]', '3.0.0b1')
+APPTOOLS = etsdep('AppTools', '3.0.0b2')  # import of enthought.io in enthought.developer
+ENTHOUGHTBASE = etsdep('EnthoughtBase', '3.0.0b2')
+ENTHOUGHTBASE_UI = etsdep('EnthoughtBase[ui]', '3.0.0b2')  # e.util.wx.* imported in developer
+ENVISAGECORE = etsdep('EnvisageCore', '3.0.0b2')
+ENVISAGEPLUGINS = etsdep('EnvisagePlugins', '3.0.0b2')  # -- imported only in enthought.gotcha
+TRAITSBACKENDWX = etsdep('TraitsBackendWX', '3.0.0')  # -- imported mostly by enthought.developer, but one from enthought.gotcha
+TRAITSGUI = etsdep('TraitsGUI', '3.0.0')
+TRAITSGUI_DOCK = etsdep('TraitsGUI[dock]', '3.0.0')  # -- imported only by enthought.developer.
+TRAITS_UI = etsdep('Traits[ui]', '3.0.0')
 
 
 # A dictionary of the pre_setup information.

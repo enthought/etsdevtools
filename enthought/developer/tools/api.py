171fba676ed5f4bc98b4429c430c5fd8b8b884ae
vibha <vibha@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-08-15 10:21:09 +0000

    Adding api.py which contains commonly used imports.

---
+++ enthought/developer/tools/api.py
@@ -0,0 +1,44 @@
+#-------------------------------------------------------------------------------
+#  
+#  Enthought developer tools.
+#
+#  These tools are intended to help developers debug and test Traits-based
+#  code running within an application framework. 
+#  
+#  Package owner/architect: David C. Morrill
+#  
+#  Date: 06/25/2006
+#  
+#  (c) Copyright 2006 by David C. Morrill
+#  
+#  NOTE: Modifications to the contents of this package in the Enthought SVN 
+#  repository should only be made with the approval of the package owner.
+#  Unapproved changes are subject to immediate reversion of the affected files.
+#  This is to ensure consistency in the package development and to allow for
+#  feature documentation and tracking.
+#  
+#-------------------------------------------------------------------------------
+# Commonly used imports.
+
+from universal_inspector import UniversalInspector
+from object_source import ObjectSource
+from class_browser import ClassBrowser    
+from favorites_browser import FavoritesBrowser
+from ui_debugger import UIDebugger
+from file_space import FileSpace
+from file_browser import FileBrowser
+from syntax_checker import SyntaxChecker
+from view_tester import ViewTester
+from traceback_viewer import TracebackViewer
+from fbi_viewer import FBIViewer
+from traits_ui_db import TraitsUIDB
+from app_monitor import AppMonitor
+from logger import Logger
+from wiretap import Wiretap
+from listener import Listener
+from object_viewer import ObjectViewer
+from profiler import Profiler
+from profile_viewer import ProfileViewer
+# FIXME: This one seems to be based on Envisage 2 and needs to be upgraded to
+# Envisage 3.
+#from envisage_browser.browser import ApplicationBrowser

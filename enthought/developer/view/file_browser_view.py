2bd0d099fa09ce0eef17c981d1454d388af422a1
vibha <vibha@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-02-17 23:39:33 +0000

    Safety check-in: View for the FileBrowser plugin.

---
+++ enthought/developer/view/file_browser_view.py
@@ -0,0 +1,148 @@
+""" A view containing a file browser. """
+
+
+# Standard library imports.
+import logging
+import glob
+
+from os \
+    import listdir, sep
+
+from os.path \
+    import exists, join, isdir, isfile, splitext
+    
+# Enthought library imports.
+from enthought.envisage.api import IExtensionRegistry
+from enthought.envisage.api import ExtensionPoint
+from enthought.pyface.workbench.api import View
+from enthought.traits.ui.menu import Action as TraitsAction, Menu as TraitsMenu
+from enthought.developer.tools.file_browser import FileBrowser, RootNode, PathNode, FileNode
+from enthought.pyface.api import GUI
+from enthought.traits.api import Instance, Property, implements, Dict, Any
+from enthought.traits.ui.api import Handler, View as TraitsView, Item, TreeEditor, InstanceEditor
+from enthought.traits.ui.tree_node import ObjectTreeNode
+
+# Setup a logger for this module.
+logger = logging.getLogger(__name__)
+
+class ObjectFileNode(ObjectTreeNode):
+    """
+    """
+    
+    node_for = [FileNode]
+    
+    label = 'name'
+    
+#    def get_icon(self, object, is_expanded):
+#        if splitext(object.file_name)[1] == '.py':
+#            return 'python'
+#        else:
+#            return super(ObjectFileNode, self).get_icon(object, is_expanded)
+    
+    def get_menu(self, object):
+        """
+        """
+        
+        actions = [TraitsAction(name = 'Open',
+                                action = 'open')]
+        
+        if splitext(object.file_name)[1] == '.py':
+            actions.append(TraitsAction(name = 'Run',
+                                        action = 'run'))
+        menu = TraitsMenu( *actions )
+        return menu
+        
+#-------------------------------------------------------------------------------
+#  Tree editor definition:
+#-------------------------------------------------------------------------------
+    
+class FileBrowserHandler(Handler):
+    
+    info = Any
+    
+    def init_info(self, info):
+        self.info = info
+        
+    def run(self, info, object):
+        self.info.object.run(object.file_name)
+        
+tree_editor = TreeEditor(
+    editable  = False,
+    selected  = 'selected',
+    nodes     = [
+        ObjectTreeNode( node_for   = [ RootNode ],
+                        auto_open  = True,
+                        label      = 'path' ),
+        ObjectTreeNode( node_for   = [ PathNode ],
+                        auto_close = True,
+                        label      = 'name' ),
+        ObjectFileNode()
+    ]
+)
+    
+class FileBrowserView(View, FileBrowser):
+    """ A view containing a file browser. """
+
+    #### 'IView' interface ####################################################
+    
+    # The part's globally unique identifier.
+    id = 'enthought.developer.tools.file_browser'
+
+    # The part's name (displayed to the user).
+    name = 'Workspace'
+
+    # The default position of the view relative to the item specified in the
+    # 'relative_to' trait.
+    position = 'bottom'
+
+    #### 'IExtensionPointUser' interface ######################################
+
+    traits_view = TraitsView( 
+        Item( name       = 'root', 
+              editor     = tree_editor,
+              show_label = False
+              ), handler=FileBrowserHandler
+              )
+    
+    ###########################################################################
+    # 'View' interface.
+    ###########################################################################
+
+    def create_control(self, parent):
+        """ Creates the toolkit-specific control that represents the view. """
+
+
+        self.ui = self.edit_traits(parent=parent, kind='subpanel')
+        # Register the file browser as a service.
+        self.window.application.register_service(FileBrowser, self)
+        return self.ui.control
+    
+    def run(self, filename=None, shell=None):
+        """
+        """
+        if filename is None:
+            filename = self.path
+            
+        if shell is None:
+            shell = self.window.get_view_by_id(
+                'enthought.plugins.python_shell_view'
+            )
+
+        if shell is not None:
+            self.run_dir(shell, filename)
+        
+    def run_dir(self, view, path):
+        if isfile(path):
+            self.run_script(view, path)
+        elif isdir(path):
+            for file in glob.iglob(join(path, '*.py')):
+                self.run_script(view, file)
+        for dir in glob.iglob(join(path, '*') + sep):
+            self.run_dir(view, dir)
+        return
+            
+    def run_script(self, view, filename):
+        view.execute_command('%run  ' + '"%s"' % filename, hidden=False)
+
+
+#### EOF ######################################################################

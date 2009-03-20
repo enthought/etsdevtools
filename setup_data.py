971405a66dfb136db4e1f885320f105e2017c61b
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-03-20 22:19:18 +0000

    * Smallest possible version number bump after making a release. * Updated dependency versions to match what is currently (or will soon be, after I finish checking everything in) in the trunk of other projects.

--- setup_data.py
+++ setup_data.py
@@ -14,15 +14,15 @@ def etsdep(p, min, max=None, literal=False):
 
 
 # Declare our ETS project dependencies.
-APPTOOLS = etsdep('AppTools', '3.2.0')  # import of enthought.io in enthought.developer
-ENTHOUGHTBASE = etsdep('EnthoughtBase', '3.0.2')
-ENTHOUGHTBASE_UI = etsdep('EnthoughtBase[ui]', '3.0.2')  # e.util.wx.* imported in developer
-ENVISAGECORE = etsdep('EnvisageCore', '3.1.0')
-ENVISAGEPLUGINS = etsdep('EnvisagePlugins', '3.1.0')  # -- imported only in enthought.gotcha
-TRAITSBACKENDWX = etsdep('TraitsBackendWX', '3.1.0')  # -- imported mostly by enthought.developer, but one from enthought.gotcha
-TRAITSGUI = etsdep('TraitsGUI', '3.0.4')
-TRAITSGUI_DOCK = etsdep('TraitsGUI[dock]', '3.0.4')  # -- imported only by enthought.developer.
-TRAITS_UI = etsdep('Traits[ui]', '3.1.0')
+APPTOOLS = etsdep('AppTools', '3.2.1')  # import of enthought.io in enthought.developer
+ENTHOUGHTBASE = etsdep('EnthoughtBase', '3.0.3')
+ENTHOUGHTBASE_UI = etsdep('EnthoughtBase[ui]', '3.0.3')  # e.util.wx.* imported in developer
+ENVISAGECORE = etsdep('EnvisageCore', '3.1.1')
+ENVISAGEPLUGINS = etsdep('EnvisagePlugins', '3.1.1')  # -- imported only in enthought.gotcha
+TRAITSBACKENDWX = etsdep('TraitsBackendWX', '3.1.1')  # -- imported mostly by enthought.developer, but one from enthought.gotcha
+TRAITSGUI = etsdep('TraitsGUI', '3.0.5')
+TRAITSGUI_DOCK = etsdep('TraitsGUI[dock]', '3.0.5')  # -- imported only by enthought.developer.
+TRAITS_UI = etsdep('Traits[ui]', '3.1.1')
 
 
 # A dictionary of the pre_setup information.
@@ -67,6 +67,6 @@ INFO = {
         TRAITS_UI,
         ],
     'name': 'ETSDevTools',
-    'version': '3.0.2',
+    'version': '3.0.3',
     }
 
